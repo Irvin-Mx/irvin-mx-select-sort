d81ad841ac9c4e94799c5d4439267cf13e8b8f15
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-29 14:41:42 +0000

    clean app.js

--- src/app.js
+++ src/app.js
@@ -5,47 +5,52 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  let randomCardsArray = []
-  const iconsObjectsArrays = [{ icon: "♦", color: "red" }, { icon: "♥", color: "red" }, { icon: "♠", color: "black" }, { icon: "♣", color: "black" }]
-  let drawClickFlag = false
-  //Retreive the input element
-  const numberOfCardsInputElement = document.querySelector("#number-of-cards");
-  const drawButtonElement = document.querySelector("#draw-button")
+  let randomCardsArray = [];
+  const iconsObjectsArrays = [
+    { icon: "♦", color: "red" },
+    { icon: "♥", color: "red" },
+    { icon: "♠", color: "black" },
+    { icon: "♣", color: "black" },
+  ];
 
-  //Add an event listener to the input element
+  // Retrieve the input element
+  const numberOfCardsInputElement = document.querySelector("#number-of-cards");
+  const drawButtonElement = document.querySelector("#draw-button");
+
+  // Add an event listener to the input element
   drawButtonElement.addEventListener("click", function (event) {
-    //console.log(randomCardsArray);
-    //console.log(numberOfCardsInputElement.value);
-  
-    randomCardsArray = []; 
+
+    // Clear the container and reset the array before rendering new cards
+    randomCardsArray = [];
     const divCardContainerElement = document.querySelector("#random-user-cards");
-    divCardContainerElement.innerHTML = ""; 
-  
-    
+    divCardContainerElement.innerHTML = "";
+
+    // Clear the log container when pressing "Draw"
     const divCardLogContainer = document.querySelector("#random-user-cards-log");
-    divCardLogContainer.innerHTML = ""; 
-  
+    divCardLogContainer.innerHTML = "";
+
+    // Reset the logArray to ensure only the current draw's logs are stored
     logArray.length = 0; 
-  
+
     let userInputNumberOfCards = numberOfCardsInputElement.value;
+
     let numberOfTotalCards = parseInt(userInputNumberOfCards);
-  
-    
+
     for (let i = 0; i < numberOfTotalCards; i++) {
+
       let randomNumberSuite = Math.floor(Math.random() * iconsObjectsArrays.length);
       let randomNumberCard = Math.floor(Math.random() * 12);
       let iconObjectWinner = iconsObjectsArrays[randomNumberSuite];
       let iconWinner = iconObjectWinner.icon;
-  
+
       randomCardsArray.push({
         number: randomNumberCard,
         icon: iconWinner,
         iconColor: iconObjectWinner.color,
       });
     }
-  
-    //console.log(randomCardsArray);
-  
+
+
     // Render random cards
     randomCardsArray.forEach((item) => {
       divCardContainerElement.innerHTML += `<div class="card mx-2">
@@ -54,11 +59,11 @@ window.onload = function () {
             ${item.icon}
           </span>
          </div>
-  
+
          <div class="card-number">
           <span class="number-span" id="random-number">${item.number}</span>
          </div>
-  
+
          <div class="card-bottom">
             <span class="icon-span rotated">
             ${item.icon}
@@ -68,24 +73,28 @@ window.onload = function () {
     });
   });
 
-  const logArray = []
-  let divCardLogContainer = document.querySelector("#random-user-cards-log")
+  const logArray = [];
+  let divCardLogContainer = document.querySelector("#random-user-cards-log");
 
-  //Order random cards array
-  const bubbleSort = (arr) => {
-    let wall = arr.length - 1; //iniciamos el wall o muro al final del array
-    while (wall > 0) {
-      for (let i = 0; i < wall; i++) {
-        if (arr[i].number > arr[i + 1].number) {
-          let aux = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = aux;
+  // Order random cards array using selection sort
+  const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let minIndex = i;
 
-          // Logs
-          logArray.push([...arr])
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[minIndex].number > arr[j].number) {
+          minIndex = j;
         }
       }
-      wall--; 
+
+      if (i !== minIndex) {
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+
+        // Log the current state of the array after each swap
+        logArray.push([...arr]);
+      }
     }
     return arr;
   };
@@ -94,37 +103,39 @@ window.onload = function () {
 
   sortButtonElement.addEventListener("click", function () {
     let randomCardsArrayCopy = [...randomCardsArray];
-    const sortedCardsArrayCopy = bubbleSort(randomCardsArrayCopy);
-    //console.log(sortedCardsArrayCopy);
-  
+    const sortedCardsArrayCopy = selectionSort(randomCardsArrayCopy);
+   
+
     divCardLogContainer.innerHTML = "";
-  
-    // Loop through the array of logs
+
     logArray.forEach((item, index) => {
-      //console.log(item);
-  
+     
+
+      // Create a container for the current log step
       let createdContainer = document.createElement("div");
       createdContainer.className = "m-2 d-flex individual-log-container-div";
-      createdContainer.innerHTML += `<span class="me-3">${index}</span>`;
-  
-      // Render cards 
+
+      createdContainer.innerHTML += `<span class="me-3">${index + 1}</span>`;
+
       item.forEach((item2) => {
         let cardHTML = `<div class="card mx-2">
-            <div class="card-top">
+             <div class="card-top">
               <span class="icon-span" id="random-icon">${item2.icon}</span>
-            </div>
-            <div class="card-number">
+             </div>
+
+             <div class="card-number">
               <span class="number-span" id="random-number">${item2.number}</span>
-            </div>
-            <div class="card-bottom">
-              <span class="icon-span rotated">${item2.icon}</span>
-            </div>
-          </div>`;
+             </div>
+
+             <div class="card-bottom">
+                <span class="icon-span rotated">${item2.icon}</span>
+             </div>
+         </div>`;
         createdContainer.innerHTML += cardHTML;
       });
-  
+
+      // Append the container to the log container
       divCardLogContainer.appendChild(createdContainer);
     });
   });
-
 };
